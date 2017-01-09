3183fa07711b601054b82686bc53a84a509bca16
dante993 <danteejmg@gmail.com>
2017-01-09 01:46:43 -0500

    Inicio del proyecto

---
+++ 4gl_entorno/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\edison\dropbox\decimo a\lenguaje 4gl\proyecto\4gl_entorno\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
